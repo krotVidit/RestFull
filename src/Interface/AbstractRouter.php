aa2ab49197e773449cad25abd49a5066fd8dbabb
krotVidit <backendkid@mail.ru>
2025-06-16 02:01:39 +0300

    [ADD] Добавлен абстрактный класс который описывает поведение

---
+++ src/Interface/AbstractRouter.php
@@ -0,0 +1,36 @@
+<?php
+
+abstract class AbstractClass {
+  
+  protected mysqli $connect;
+
+  public function __construct(mysqli $connect)
+  {
+        $this->connect = $connect;
+        $this->connectApi();
+  }
+
+  protected function connectApi () : void
+  {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Headers: *');
+        header('Access-Control-Allow-Methods: *');
+        header('Access-Control-Allow-Credentials: true');
+        header('Content-Type: application/json');
+  }
+
+  abstract public function getHandler (string $method, string $path);
+
+  abstract protected function handler (string $method, string $path);
+
+  abstract protected function handleGet (string $type, int $id);
+
+  abstract protected function handlePost (string $type);
+
+  abstract protected function  handlePatch(string $type, int $id);
+
+  abstract protected function handlePut(string $type, int $id);
+
+  abstract protected function handleDelete(string $int, int $id);
+
+}
