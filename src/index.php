6ae857f7c6c0733a6b568a7d34b2144cb9ff55ca
krotVidit <backendkid@mail.ru>
2025-06-19 23:19:00 +0300

    [mini-fix]

--- src/index.php
+++ src/index.php
@@ -4,8 +4,6 @@ namespace App;
 
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/connect.php';
-/* $response = file_get_contents('https://jsonplaceholder.typicode.com/posts'); */
-/* $posts = json_decode($response, true); */
 
 use App\Post\Router;
 
